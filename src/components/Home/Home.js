36ad00fcb3ad00dd4ed7d2005712fe9385546980
mdaias <mdaias29@gmail.com>
2022-04-05 05:13:32 +0600

    create CustomLink,data.json,hooks and diclare Routes

---
+++ src/components/Home/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <section className='container'>
+                <div className="details-part row">
+                    
+                </div>
+                <div className="img-part row">
+
+                </div>
+            </section>
+            <section>
+
+            </section>
+        </div>
+    );
+};
+
+export default Home;
